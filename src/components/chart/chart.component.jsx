b9673684e394e8530f15a2dea01957c379c32b5d
Rafalstrus <rafal.strus@onet.eu>
2021-06-17 17:17:16 +0200

    charts are working now except pln

--- src/components/chart/chart.component.jsx
+++ src/components/chart/chart.component.jsx
@@ -3,21 +3,29 @@ import React from 'react';
 import './chart.styles.css';
 import {Line } from 'react-chartjs-2';
 
+function changeData(props){
+  state.datasets[0].data =[]
+  state.labels = []
+    props.chartData.rates.forEach(function (item, index) {
+    console.log(item['effectiveDate'],item['mid']);
+    state.datasets[0].data.push(item['mid'])
+    state.labels.push(item['effectiveDate'])
+})}
+
 const state = {
-    labels: ['January', 'February', 'March',
-             'April', 'May'],
-    datasets: [
-      {
-        label: 'Rainfall',
-        fill: false,
-        backgroundColor: 'rgba(75,192,192,1)',
-        borderColor: 'red',
-        borderWidth: 2,
-        data: [65, 59, 80, 81, 56]
-      }
-    ]
-  }
-  
+  labels: [],
+  datasets: [
+    {
+      label: 'Rainfall',
+      fill: false,
+      backgroundColor: 'rgba(75,192,192,1)',
+      borderColor: 'red',
+      borderWidth: 2,
+      data: []
+    }
+  ]
+}
+
 export const ChartContainer = props =>(
     <div id="chart">
         <Line 
@@ -31,10 +39,11 @@ export const ChartContainer = props =>(
             }
           }}
         />
-        <p>{console.log(props.chartData.rates)}</p>
-        {}
-        <p>{console.log(props.chartData.rates)}</p>
+          {(props.chartData.rates === undefined) ? '' :changeData(props)}
     </div>
 
 )
+
+
+
 
